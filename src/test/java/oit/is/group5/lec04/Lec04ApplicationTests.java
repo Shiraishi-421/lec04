78aea63149d96ae49faa78d050c4c802d409a461
Shiraishi-421 <e1b20052@oit.ac.jp>
2022-10-18 14:10:47 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/group5/lec04/Lec04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group5.lec04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lec04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
